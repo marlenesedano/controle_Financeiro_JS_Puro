5055777c04833dc7eaaba91cb1e9d72b0e0220c4
marlenesedano <marlenesedanodasilva@gmail.com>
2020-12-30 17:11:44 -0400

    atualização e remoção de registros

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 const transactionsUl = document.querySelector('#transactions')
+const incomeDisplay = document.querySelector('#money-plus')
+const expenseDisplay = document.querySelector('#money-minus')
+const balanceDisplay = document.querySelector('#balance')
+const form = document.querySelector('#form')
+const inputTransactionName = document.querySelector('#text')
+const inputTransactionAmount = document.querySelector('#amount')
 
-const dummyTransactions = [
-  {id: 1, name: 'Bolo', amount: -20},
-  {id: 2, name: 'Salario', amount: 300},
-  {id: 3, name: 'Salame', amount: -20},
-  {id: 4, name: 'Violão', amount: 150}
-]
+
+
+const localStorageTransactions = JSON.parse(localStorage
+  .getItem('transactions'))
+let transactions = localStorage
+.getItem('transactions') !== null ? localStorageTransactions : []
+
+const removeTransaction = ID => {
+  transactions = transactions.filter(transaction => transaction.id !== ID)
+  init()
+}
+
+
 // função para inserir os elementos na DOM
 const addTransactionIntoDOM = transaction =>{
   const operator = transaction.amount < 0 ? '-' : '+'
@@ -15,21 +28,64 @@ const addTransactionIntoDOM = transaction =>{
   
   li.classList.add(CSSClass);
   li.innerHTML = `
-    ${transaction.name} <span> ${operator} R$ ${amountWithoutOperator} </span><button class = "delete-btn">x</button>
+    ${transaction.name} 
+    <span> ${operator} R$ ${amountWithoutOperator} </span>
+    <button class = "delete-btn" onClick = "removeTransaction(${transaction.id})">
+      x
+    </button>
   `
-  console.log(li);
   transactionsUl.prepend(li) // o metodo append inseri o elemento que ele recebeu como argumento
 }
+
 const updateBalanceValues = () => {
-  const transactionsAmounts = dummyTransactions.map(transaction => transaction.amount)
-  const total = transactionsAmounts.reduce((accumulator, transaction) => accumulator + transaction, 0).toFixed(2) // to fixed, informa a quantidade de decimais
-  console.log(total)
+  const transactionsAmounts = transactions.map(transaction => transaction.amount)
+  const total = transactionsAmounts
+  .reduce((accumulator, transaction) => accumulator + transaction, 0)
+  .toFixed(2) // to fixed, informa a quantidade de decimais
+  const income = transactionsAmounts
+  .filter(value => value > 0)
+  .reduce((accumulator, value) => accumulator + value, 0)
+  .toFixed(2)
+  const expense = Math.abs( transactionsAmounts
+  .filter(value => value < 0)
+  .reduce((accumulator, value) => accumulator + value, 0))
+  .toFixed(2)
+  
+  balanceDisplay.textContent = `R$ ${total}`
+  incomeDisplay.textContent = `R$ ${income}`
+  expenseDisplay.textContent = `R$ ${expense}`
   
 
 }
 
 const init = () => {
-  dummyTransactions.forEach(addTransactionIntoDOM)
+  transactionsUl.innerHTML = ''
+  transactions.forEach(addTransactionIntoDOM)
   updateBalanceValues()
 }
 init()
+//gerar ids aleatorios
+
+const generateID = () => Math.round(Math.random() * 1000)
+
+form.addEventListener('submit', event => {
+  event.preventDefault()
+  const transactionName = inputTransactionName.value.trim()
+  const transactionAmount = inputTransactionAmount.value.trim()
+
+  if(transactionName === '' || transactionAmount === ''){
+    alert('Por favor, preencha tanto o novo quanto o valor da transação')
+    return
+  }
+  const transaction = {
+    id: generateID(),
+    name: transactionName,
+    amount: Number(transactionAmount)
+  }
+
+  transactions.push(transaction)
+  init()
+  inputTransactionName.value = ''
+  inputTransactionAmount.value = ''
+
+})
